94662ee1835c7b45116379dc0023d31d5a0a126b
genady <puhovkostya1@gmail.com>
2024-08-05 20:07:01 +0300

    started dev

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import { Button } from "@/components/ui/button";
+
 
 
 export default function Home() {
   return (
     <>
     <h1>HOME</h1>
-    <Button variant="outline">bascket</Button>
     </>
   );
 }
