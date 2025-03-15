9e0a6ea1cd98ff8a9fd054ce2c12bc8ceea8d9a6
genady <puhovkostya1@gmail.com>
2025-03-15 17:35:39 +0300

    db scheme is ready

---
+++ prisma/seed.ts
@@ -0,0 +1,14 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+
+async function generateData(){
+    
+}
+
+async function cleanData(){
+    
+}
+
+async function main(){
+    await generateData()
+    await cleanData()
+}
